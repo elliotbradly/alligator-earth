13c5ec415992321c40e1cd110c2a5cb5ae4382fd
elliotbradly <elliotbradly@gmail.com>
2023-10-25 12:34:41 -0400

    controllers working

--- src/pages/GameScreen.vue
+++ src/pages/GameScreen.vue
@@ -20,13 +20,46 @@ import { useRouter, useRoute } from 'vue-router'
 
 import { Terminal } from 'xterm';
 
+import { Mouse, Keyboard, Gamepad, or, and } from 'contro'
+
 const router = useRouter()
 const route = useRoute()
 
 const instance = getCurrentInstance();
 const SHADE = inject('SHADE')
 
+const keyboard = new Keyboard()
+const gamepad = new Gamepad()
+
 onMounted(async () => {
+
+  const controls = {
+  jump: or(gamepad.button('A').trigger, keyboard.key('Space').trigger),
+  menu: or(gamepad.button('Back').trigger, keyboard.key('Esc').trigger),
+  inventory: or(gamepad.button('LB').trigger, keyboard.key('E').trigger),
+  map: or(gamepad.button('RB').trigger, keyboard.key('M').trigger),
+  statusOverlay: or(gamepad.button('RB'), keyboard.key('Tab')),
+}
+
+function gameLoop() {
+
+  console.log("looping")
+  // Update the UI to reflect the player's input device(s)
+  //game.jumpButton.text = controls.jump.label
+  //game.menuButton.text = controls.menu.label
+  // ...
+
+  // Query the controls and do something
+  if (controls.jump.query()) alert("jump")
+  if (controls.menu.query()) alert("menu")
+  //game.statusOverlay.visible = controls.statusOverlay.query()
+  // ...
+
+  requestAnimationFrame(gameLoop)
+}
+
+
+gameLoop()
 
 
   var term = new Terminal();
