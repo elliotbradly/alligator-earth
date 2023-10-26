8506681b109d3d86dd0c8ade052f691e7f95654e
elliotbradly <elliotbradly@gmail.com>
2023-10-25 22:10:28 -0400

    character sheet

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -11,6 +11,10 @@
     >
 
     <q-list bordered separator>
+
+
+
+
       <q-item clickable v-ripple :active="active">
         <q-item-section avatar>
           <q-avatar rounded>
@@ -55,6 +59,23 @@
 
 
     <q-list bordered padding>
+
+      <q-item clickable v-ripple >
+      <q-item-section side>
+        <q-avatar rounded size="48px">
+          <img src="https://cdn.quasar.dev/img/avatar.png" />
+          <q-badge floating color="teal">new</q-badge>
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Mary</q-item-label>
+        <q-item-label caption>2 new messages</q-item-label>
+      </q-item-section>
+      <q-item-section side>
+        3 min ago
+      </q-item-section>
+    </q-item>
+
       <q-item-label header>User Controls</q-item-label>
 
       <q-item clickable v-ripple>
