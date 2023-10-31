f35e4cbf4316b4eadca62801216a99833bfa18b3
elliotbradly <elliotbradly@gmail.com>
2023-10-31 19:16:42 -0400

    creating some screens

--- src/composables/screens.ts
+++ src/composables/screens.ts
@@ -1,7 +1,55 @@
+import { ref, onMounted, onUnmounted, onUpdated, inject, getCurrentInstance } from 'vue'
+
+import * as ActVsg from '../110.shade/01.visage.unit/visage.action'
+import * as ActCan from '../110.shade/03.container.unit/container.action'
+
+
+
 export type HelloWorld = string | number
 
-export const mount = (value: HelloWorld) => {
+
+export const mount = async (value: HelloWorld) => {
     console.log('sampleFunc:: ', value)
+
+    const instance = getCurrentInstance();
+    const SHADE = inject('SHADE')
+
+    var bit = await SHADE['hunt']( ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { } });
+    instance?.proxy?.$forceUpdate();
 
     return value
 }
+
+export const update = async (value: HelloWorld) => {
+  console.log('sampleFunc:: ', value)
+
+  const instance = getCurrentInstance();
+  const SHADE = inject('SHADE')
+
+  var bit = await SHADE['hunt']( ActVsg.REMOVE_VISAGE, { idx: "vsg00" })
+  bit = await SHADE['hunt']( ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: { } })
+
+  bit = await SHADE['hunt']( ActVsg.READ_VISAGE, { idx: "vsg00" })
+
+  bit = await SHADE['hunt']( ActCan.WRITE_CONTAINER, { idx: "can00", src: 'vsg00' })
+  bit = await SHADE['hunt']( ActCan.SURFACE_CONTAINER, { idx: 'fce-can-00', src: "vsg00" });
+
+  return value
+}
+
+export const unmount = async (value: HelloWorld) => {
+  console.log('sampleFunc:: ', value)
+
+  const instance = getCurrentInstance();
+  const SHADE = inject('SHADE')
+
+  var bit = await SHADE['hunt']( ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: {} });
+  instance?.proxy?.$forceUpdate();
+
+  return value
+}
+
+export type Shade <Type> = {
+  hunt: Function;
+} & Type
+
--- src/pages/004.game-play.vue
+++ src/pages/004.game-play.vue
@@ -16,7 +16,7 @@ import * as ActShd from '../110.shade/00.shade.unit/shade.action'
 import * as ActVsg from '../110.shade/01.visage.unit/visage.action'
 import * as ActCan from '../110.shade/03.container.unit/container.action'
 
-import {mount } from "../composables/screens"
+import {mount, update, unmount } from "../composables/screens"
 
 const router = useRouter()
 const route = useRoute()
@@ -26,9 +26,6 @@ const SHADE = inject('SHADE')
 
 onMounted(async () => {
 
-  var bit = await SHADE.hunt(SHADE.ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: {} });
-  instance?.proxy?.$forceUpdate();
-
   mount('on')
 
   //alert(window.electron.store.get('foo'));
@@ -37,20 +34,15 @@ onMounted(async () => {
 onUpdated(async () => {
   // text content should be the same as current `count.value`
 
-  var bit = await SHADE.hunt( ActVsg.REMOVE_VISAGE, { idx: "vsg00" })
-  bit = await SHADE.hunt( ActVsg.MOUNT_VISAGE, { idx: "vsg00", src: "indexCanvas", dat: {} })
+  update('on')
 
-  bit = await SHADE.hunt( ActVsg.READ_VISAGE, { idx: "vsg00" })
-
-  bit = await SHADE.hunt( ActCan.WRITE_CONTAINER, { idx: "can00", src: 'vsg00' })
-  bit = await SHADE.hunt( ActCan.SURFACE_CONTAINER, { idx: 'fce-can-00', src: "vsg00" });
 
 })
 
 onUnmounted(async () => {
 
-  //console.log("unmounted..")
-  var bit = await SHADE.hunt(SHADE.ActVsg.REMOVE_VISAGE, { idx: "vsg00" })
+  unmount('on')
+
 
 })
 
