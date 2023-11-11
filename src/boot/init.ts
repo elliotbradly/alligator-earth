bb164ebbb4ce586420f5a6561ac7a36f5609da88
elliotbradly <elliotbradly@gmail.com>
2023-11-11 18:25:02 -0500

    control connected

--- src/boot/init.ts
+++ src/boot/init.ts
@@ -12,6 +12,7 @@ export default boot(async ( dat ) => {
 
   var win: any = window
   dat.app.provide('SHADE', win.SHADE)
+  dat.app.provide('CONTROL', win.CONTROL)
   dat.app.provide('MQTT', win.MQTT)
 
   const prt = 8883;
@@ -19,7 +20,6 @@ export default boot(async ( dat ) => {
   const localBit = { idx: 'local', src: local };
 
   var bit = await win.SHADE.hunt( win.SHADE.ActShd.INIT_SHADE, { val: 1, dat: win.MQTT, src: local });
-
-
+  var bit = await win.CONTROL.hunt( win.CONTROL.ActCtl.INIT_CONTROL, { val: 1, dat: win.MQTT, src: local });
 
 })
